8f1be357159ed9f27ecc3b5865a3bc23477e37ea
LeThiHuynhNhu1412 <lnhu10037@gmail.com>
2024-08-07 01:10:22 +0700

    mapping,fillData/sp

--- QLBanDienThoai/src/main/java/com/qlbandienthoai/DAO/SanPhamDAO.java
+++ QLBanDienThoai/src/main/java/com/qlbandienthoai/DAO/SanPhamDAO.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.qlbandienthoai.DAO;
+
+import com.qlbandienthoai.entity.SanPham;
+import com.qlbandienthoai.utils.XJdbc;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author ADMIN
  */
 public class SanPhamDAO {
-    
+
+      public void insert(SanPham model) {
+     String insert = String.format( "INSERT INTO SanPham(MaSanPham, TenSanPham, MaHangDienThoai, MoTa, SoLuong, Gia, Hinh) VALUES(?, ?, ?, ?, ?, ?, ?)");
+                   XJdbc.update(insert,
+                    model.getCode(),
+                    model.getName(),
+                    model.getNameType(),
+                    model.getNote(),
+                    model.getQuatity(),
+                    model.getPrice(),
+                   model.getImage()
+                   );
+    }
+
+    // Method to retrieve all products with brand names
+    public List<SanPham> selectAll() {
+        List<SanPham> list = new ArrayList<>();
+        String sql = "SELECT sp.MaSanPham, sp.TenSanPham, hdt.TenHangDienThoai AS TenHangDienThoai, sp.MoTa, sp.SoLuong, sp.Gia "
+                + "FROM SANPHAM sp "
+                + "JOIN HANGDIENTHOAI hdt ON sp.MaHangDienThoai = hdt.MaHangDienThoai";
+        try {
+            ResultSet rs = XJdbc.query(sql);
+            while (rs.next()) {
+                SanPham sanPham = new SanPham();
+                sanPham.setCode(rs.getString("MaSanPham"));
+                sanPham.setName(rs.getString("TenSanPham"));
+                sanPham.setNameType(rs.getString("TenHangDienThoai"));
+                sanPham.setNote(rs.getString("MoTa")); // Set brand name
+// Set brand name
+                sanPham.setQuatity(rs.getInt("SoLuong"));
+                sanPham.setPrice(rs.getInt("Gia"));
+                list.add(sanPham);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
